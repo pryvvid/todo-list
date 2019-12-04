acacb1ffb984baaa35c421fa5bdaf10c3a3389a1
pryvvid <pryvvid@gmail.com>
2019-12-04 21:35:39 +0300

    init: todo-list

---
+++ src/AddTodo.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+  state = {
+    content: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      content: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodo(this.state);
+    this.setState({
+      content: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Add new Todo:</label>
+          <input onChange={this.handleChange} value={this.state.content} />
+          <button onSubmit={this.handleSubmit}>
+            Add
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default AddTodo;
